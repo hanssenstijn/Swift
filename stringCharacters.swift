f60117dff556e494907697998f176b8b47060e5e
Hanssen <35262249+hanssenstijn@users.noreply.github.com>
2020-02-12 13:18:41 +0100

    strings and characters
    how to combine strings and use a for loop.

---
+++ stringCharacters.swift
@@ -0,0 +1,14 @@
+// string mutability
+var variableString = "Horse"
+variableString += "and carriage"
+
+// for loop
+for character in "DOG" {
+	print(character)
+}
+
+let eAcute: character = "\u{E9}"
+
+let unusualMenagerie = "Koala, Snail, Penguin, Dromedary"
+print("unusualMenagerie has \(unusualMenagerie.count) characters")
+
